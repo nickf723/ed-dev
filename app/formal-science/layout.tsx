1c76beb44b24250540ab4e298dcc40649560c067
nickf723 <nickfarley723@gmail.com>
2026-08-07 23:08:16 -0400

    integrate formal science vocabulary trigger

--- app/formal-science/layout.tsx
+++ app/formal-science/layout.tsx
@@ -9,7 +9,10 @@ export default function FormalScienceLayout({
   return (
     <>
       {children}
-      <VocabularyDrawer scopes={formalScienceVocabularyScopes} />
+      <VocabularyDrawer
+        scopes={formalScienceVocabularyScopes}
+        hiddenTriggerPaths={["/formal-science"]}
+      />
     </>
   );
 }
