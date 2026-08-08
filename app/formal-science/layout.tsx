7c8c2461cb542c69a2e867ebbcc675bc742c8bf4
nickf723 <nickfarley723@gmail.com>
2026-08-07 22:02:47 -0400

    add formal science vocabulary layout

---
+++ app/formal-science/layout.tsx
@@ -0,0 +1,15 @@
+import VocabularyDrawer from "@/app/_components/VocabularyDrawer";
+import { formalScienceVocabularyScopes } from "@/app/_data/vocab/scopes";
+
+export default function FormalScienceLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+      <VocabularyDrawer scopes={formalScienceVocabularyScopes} />
+    </>
+  );
+}
