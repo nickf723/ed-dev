1e1b097b91a0b96c6c11f1a3730dc1d8965b24f6
nickf723 <nickfarley723@gmail.com>
2026-08-16 11:40:19 -0400

    Register Astronomy and Economics curriculum modules

--- lib/curriculum/manifest.ts
+++ lib/curriculum/manifest.ts
@@ -3,11 +3,13 @@ import { COMPUTER_SCIENCE_CURRICULUM } from "@/lib/curriculum/computer-science";
 import { GROUP_THEORY_CURRICULUM } from "@/lib/curriculum/group-theory";
 import { HISTORY_CURRICULUM } from "@/lib/curriculum/humanities/history";
 import { LOGIC_CURRICULUM } from "@/lib/curriculum/logic";
+import { ASTRONOMY_CURRICULUM } from "@/lib/curriculum/natural/astronomy";
 import { BIOLOGY_CURRICULUM } from "@/lib/curriculum/natural/biology";
 import { ELECTROMAGNETISM_CURRICULUM } from "@/lib/curriculum/natural/electromagnetism";
 import { PHYSICS_CURRICULUM } from "@/lib/curriculum/natural/physics";
 import { RELATIVITY_CURRICULUM } from "@/lib/curriculum/natural/relativity";
 import { WAVES_OPTICS_CURRICULUM } from "@/lib/curriculum/natural/waves-optics";
+import { ECONOMICS_CURRICULUM } from "@/lib/curriculum/social/economics";
 import type { CurriculumNode } from "@/lib/curriculum/types";
 
 /**
@@ -25,9 +27,11 @@ export const CURRICULUM_MODULES: readonly CurriculumNode[] = [
   LOGIC_CURRICULUM,
   COMPUTER_SCIENCE_CURRICULUM,
   BIOLOGY_CURRICULUM,
+  ASTRONOMY_CURRICULUM,
   PHYSICS_CURRICULUM,
   ELECTROMAGNETISM_CURRICULUM,
   WAVES_OPTICS_CURRICULUM,
   RELATIVITY_CURRICULUM,
+  ECONOMICS_CURRICULUM,
   HISTORY_CURRICULUM,
 ];
