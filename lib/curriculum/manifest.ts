97aad3b44ca3e05a5af14c39a62564738f8277b8
nickf723 <nickfarley723@gmail.com>
2026-08-17 19:08:43 -0400

    Register Anthropology curriculum

--- lib/curriculum/manifest.ts
+++ lib/curriculum/manifest.ts
@@ -13,6 +13,7 @@ import { ELECTROMAGNETISM_CURRICULUM } from "@/lib/curriculum/natural/electromag
 import { PHYSICS_CURRICULUM } from "@/lib/curriculum/natural/physics";
 import { RELATIVITY_CURRICULUM } from "@/lib/curriculum/natural/relativity";
 import { WAVES_OPTICS_CURRICULUM } from "@/lib/curriculum/natural/waves-optics";
+import { ANTHROPOLOGY_CURRICULUM } from "@/lib/curriculum/social/anthropology";
 import { ECONOMICS_CURRICULUM } from "@/lib/curriculum/social/economics";
 import { LINGUISTICS_CURRICULUM } from "@/lib/curriculum/social/linguistics";
 import { POLITICAL_SCIENCE_CURRICULUM } from "@/lib/curriculum/social/political-science";
@@ -40,6 +41,7 @@ export const CURRICULUM_MODULES: readonly CurriculumNode[] = [
   ELECTROMAGNETISM_CURRICULUM,
   WAVES_OPTICS_CURRICULUM,
   RELATIVITY_CURRICULUM,
+  ANTHROPOLOGY_CURRICULUM,
   ECONOMICS_CURRICULUM,
   LINGUISTICS_CURRICULUM,
   POLITICAL_SCIENCE_CURRICULUM,
