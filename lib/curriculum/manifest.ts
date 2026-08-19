be02a7037e7c6971256ff9a3b1149bff4a610764
nickf723 <nickfarley723@gmail.com>
2026-08-19 00:50:44 -0400

    Register focused Number Theory curriculum

--- lib/curriculum/manifest.ts
+++ lib/curriculum/manifest.ts
@@ -4,6 +4,7 @@ import { DATA_SCIENCE_CURRICULUM } from "@/lib/curriculum/data-science";
 import { GEOMETRY_CURRICULUM } from "@/lib/curriculum/geometry";
 import { GROUP_THEORY_CURRICULUM } from "@/lib/curriculum/group-theory";
 import { INFORMATION_SCIENCE_CURRICULUM } from "@/lib/curriculum/information-science";
+import { NUMBER_THEORY_CURRICULUM } from "@/lib/curriculum/number-theory";
 import { STATISTICS_CURRICULUM } from "@/lib/curriculum/statistics";
 import { SYSTEMS_SCIENCE_CURRICULUM } from "@/lib/curriculum/systems-science";
 import { AGRICULTURE_CURRICULUM } from "@/lib/curriculum/applied/agriculture";
@@ -65,6 +66,7 @@ export const CURRICULUM_MODULES: readonly CurriculumNode[] = [
   REFINED_ALGEBRA_CURRICULUM,
   GROUP_THEORY_CURRICULUM,
   GEOMETRY_CURRICULUM,
+  NUMBER_THEORY_CURRICULUM,
   STATISTICS_CURRICULUM,
   LOGIC_CURRICULUM,
   COMPUTER_SCIENCE_CURRICULUM,
