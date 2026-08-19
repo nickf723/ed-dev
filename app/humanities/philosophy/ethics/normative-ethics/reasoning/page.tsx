4931b291d98dfb0fa92ae1a5e07a5b56fea9e945
nickf723 <nickfarley723@gmail.com>
2026-08-19 17:11:03 -0400

    Fix Ethical Reasoning route import

--- app/humanities/philosophy/ethics/normative-ethics/reasoning/page.tsx
+++ app/humanities/philosophy/ethics/normative-ethics/reasoning/page.tsx
@@ -1,4 +1,4 @@
-import EthicalReasoningExperience from "../../../EthicalReasoningExperience";
+import EthicalReasoningExperience from "../../EthicalReasoningExperience";
 
 export default function EthicalReasoningPage() {
   return <EthicalReasoningExperience />;
